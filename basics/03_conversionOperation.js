35cb9b0de57f82bbe6d70fa481872211e1f532aa
Rohit-github21 <153302579+Rohit-github21@users.noreply.github.com>
2023-12-12 16:20:48 +0000

    conversionOperation

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,64 @@
+let score = "Rohit"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; False => 0;
+
+let isLoggedIn = "rohit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof isLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Rohit" => true
+// any number like 7895 => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*************** Operation *****************
+
+let value = 4
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello" 
+let str2 = " rohit"
+
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1 + 2);
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 200
+++gameCounter;
+console.log(gameCounter);
