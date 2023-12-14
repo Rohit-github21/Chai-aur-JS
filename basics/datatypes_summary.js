4e0bb480b7c20579ad1dfca2494d0602da3d21e3
Rohit-github21 <153302579+Rohit-github21@users.noreply.github.com>
2023-12-14 03:38:53 +0000

    datatype summary done

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol,BigInt,
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 13216516513169151651845n;
+
+// Referance (Non-Primitive)
+
+// Array, Objects,Functions
+
+const heros = ["shaktiman","naagraj"]
+
+ let myObj = {
+    name: "Rohit",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof anotherId);
